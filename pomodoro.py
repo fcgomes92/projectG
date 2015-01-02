4b45146bc730a0921024033dae1131ca73b433d5
gomes <fcgomes.92@gmail.com>
2015-01-02 21:39:25 -0200

    Second commit

---
+++ pomodoro.py
@@ -0,0 +1,18 @@
+#/usr/env python
+# ProjectG - Pomodoro Timer
+# Interface : terminal
+# author: gomes
+
+class timer():
+	def __init__(self, begin, end):
+		self.begin = begin
+		self.end = end
+		pass
+
+	def start(self):
+		# Start timer
+		pass
+
+	def stop(self):
+		# Stop timer
+		pass
