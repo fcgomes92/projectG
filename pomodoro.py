6b57a93c85dfb0cfbee0a7c8d24dc1e6dbc89ec3
gomes <fcgomes.92@gmail.com>
2015-01-02 22:32:39 -0200

    Primeira estrutura do timer para o pomodoro.

--- pomodoro.py
+++ pomodoro.py
@@ -3,16 +3,34 @@
 # Interface : terminal
 # author: gomes
 
+from time import time, sleep
+from datetime import datetime, timedelta
+import os
+
 class timer():
 	def __init__(self, begin, end):
 		self.begin = begin
 		self.end = end
 		pass
 
-	def start(self):
-		# Start timer
-		pass
+	def start_pomodoro(self):
+		print("Começou! Tempo inicial: " )
+		tempo = datetime.now()
+		while  self.end > tempo:
+			os.system('clear')
+			print("Tempo inicial: " + str(self.begin.time()))
+			print("Tempo corrente: " + ":".join(str(i) for i in [tempo.hour,tempo.minute,tempo.second]))
+			print("Tempo final: " + str(self.end.time()))
+			sleep(0.5)
+			tempo = datetime.now()
+			pass
 
-	def stop(self):
+	def stop_pomodoro(self):
 		# Stop timer
 		pass
+try:
+	tempo = int(input("Entre com o tempo: "))
+	t = timer(datetime.now(), (datetime.now() + timedelta(minutes=+tempo)) )
+	t.start_pomodoro()
+except Exception as e:
+	print("Erro!")
